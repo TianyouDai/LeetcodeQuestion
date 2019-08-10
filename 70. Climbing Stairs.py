cad1e481f4913b7e0cd1aa8d6e60ec03a151a729
Diana Dai <31893452+TianyouDai@users.noreply.github.com>
2019-08-09 21:14:57 -0400

    Add files via upload

---
+++ 70. Climbing Stairs.py
@@ -0,0 +1,7 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        step = [1,1]
+        for i in range(2,n+1):
+            step.append(step[i-1] + step[i-2])
+        return step[n]
+        
