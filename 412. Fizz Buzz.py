5b7a573d6cb0f5cc317e89bde87bee1fee0c87c9
Diana Dai <31893452+TianyouDai@users.noreply.github.com>
2019-11-30 18:17:31 -0500

    Add files via upload

---
+++ 412. Fizz Buzz.py
@@ -0,0 +1,15 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        fizzbuzz_list = []
+        for i in range(1, n + 1):
+            if i % 5 == 0 and i % 3 == 0:
+                fizzbuzz_list.append('FizzBuzz')
+            elif i % 3 == 0:
+                fizzbuzz_list.append('Fizz')
+            elif i % 5 == 0:
+                fizzbuzz_list.append('Buzz')
+            else:
+                print(i)
+                fizzbuzz_list.append(str(i))
+        return fizzbuzz_list
+        
